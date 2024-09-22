aeca3a9d7e6129c213871828c0dd7ba5d64b52d6
pkyu <55111674+pkyu@users.noreply.github.com>
2024-09-22 18:53:54 -0400

    routing change

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const App = () => {
         <Navbar />
         <main>
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/experience" element={<Experience />} />
             <Route path="/projects" element={<Projects />} />
